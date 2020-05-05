f092870490384d9d8635ecec64f104d3bd0247cb
Michael Oshosanya <michaeloshosanya@gmail.com>
2020-05-05 11:03:15 +0100

    Fix import

--- src/tests/insomnia.test.ts
+++ src/tests/insomnia.test.ts
@@ -1,8 +1,8 @@
-import { Insomnia } from '../exporters'
+import Insomnia from '../exporters/insomnia/insomnia'
 
 describe('Test buildQueryArgs', function() {
     it('returns empty string for empty arguments', function () {
-        let insomnia = new Insomnia()
+        let insomnia = new Insomnia();
         let result = insomnia.buildQueryArgs([]);
         expect(result).toBe("");
     });

9050f11e13ec62d9cf1408ddf7547a12315a883c
Emiliano Leite <emiliano.leite@creditas.com.br>
2020-10-29 18:49:22 -0300

    Fix typo

--- src/exporters/postman/tests/integration/convert.test.ts
+++ src/exporters/postman/tests/integration/convert.test.ts
@@ -1,5 +1,5 @@
 const introspectionResult = require('../../../tests/utils/test_introspection_result.json');
-const expectedConvertionResult = require('./expected_convertion_result.json');
+const expectedConversionResult = require('./expected_conversion_result.json');
 import Postman from '../../postman';
 
 describe('Postman', function () {
@@ -7,7 +7,7 @@ describe('Postman', function () {
     it('corretly generates a postman collection file', async function () {
       const result = (new Postman).convert(introspectionResult, 'http://localhost:3000', 'Query', 'Mutation')
 
-      expect(JSON.parse(result)).toEqual(expectedConvertionResult)
+      expect(JSON.parse(result)).toEqual(expectedConversionResult)
     })
   })
 })
